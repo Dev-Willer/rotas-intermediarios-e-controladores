d512e67788a116e9ba5d770470fc5086e77d8ef6
Dev-Willer <josewiller2018@gmail.com>
2024-07-27 16:32:06 -0300

    Teste do parametro de rota req.params

--- src/servidor.ts
+++ src/servidor.ts
@@ -12,4 +12,44 @@ servidor.get('/usuarios', (req, res) => {
 servidor.get('/') /// => Deste modo eu crio apenas a rota: ===> ISSO É UMA ROTA!!!
 
 
+
+/////////////////////////////############# BUSCANDO UM E-MAIL EM UM VETOR || COM VALIDAÇÃO ##############////////////////////////////////////////////////////
+
+const users = [
+    {nome:'Ana Maria', email:'anamaria@email.com.br'},
+    {nome:'Julia Santos', email:'juliasantos@email.com.br'},
+    {nome:'Cristina Faria', email:'cristinafaria@email.com.br'}
+]
+
+servidor.get('/users/:email', (req, res) => {
+    const {email} = req.params
+
+    const user = users.find((item => {
+        return item.email === email
+    }))
+
+    if (!user) {
+        return res.send('Pessoa não encontrada!')
+    }
+
+    return res.send(user)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 servidor.listen(process.env.PORT)
